7a5b6f3b47e6a8e6412592adfe76ac983eeaf861
Hamid Ali <hamidali.webdev@gmail.com>
2026-01-05 12:08:18 +0500

    add reactjs setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <h1 className='bg-indigo-500 px-6 py-7 text-2xl text-center'>This is heading</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
